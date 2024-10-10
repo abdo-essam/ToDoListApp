c7e848cff4e78f6f8e3241dad7549b9f81966662
abdo <abdo.e.mosa@gmail.com>
2024-10-10 17:49:31 +0300

    Design Add Task Page

--- app/src/main/java/com/ae/todolistapp/ui/MainActivity.kt
+++ app/src/main/java/com/ae/todolistapp/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ae.todolistapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import com.ae.todolistapp.R
+import com.ae.todolistapp.utils.changePage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,5 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickFab(view: View) {
+        Navigation.changePage(view, R.id.action_homePageFragment_to_addTaskFragment)
     }
 }
