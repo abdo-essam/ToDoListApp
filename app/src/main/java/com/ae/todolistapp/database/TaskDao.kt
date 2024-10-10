76ecd1310c286908a9c6d268df751b6ef7184b28
abdo <abdo.e.mosa@gmail.com>
2024-10-10 14:40:32 +0300

    Add Repository and Flow to automatically observe data changes in real-time

--- app/src/main/java/com/ae/todolistapp/database/TaskDao.kt
+++ app/src/main/java/com/ae/todolistapp/database/TaskDao.kt
@@ -3,16 +3,20 @@ package com.ae.todolistapp.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.ae.todolistapp.model.Task
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
-    @Query("SELECT * FROM task ORDER BY task_activated DESC, task_date, task_time")
-    suspend fun loadTasks(): List<Task>
 
-    @Insert
+    // Using Flow for real-time updates in loadTasks
+    @Query("SELECT * FROM task ORDER BY task_activated DESC, task_date, task_time")
+    fun loadTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)  // Optional conflict strategy
     suspend fun createTask(task: Task)
 
     @Update
@@ -24,6 +28,7 @@ interface TaskDao {
     @Delete
     suspend fun deleteTask(task: Task)
 
-    @Query("SELECT * FROM task WHERE task_title like '%' || :queryWord || '%'")
-    suspend fun search(queryWord: String): List<Task>
+    // Using Flow for real-time search results
+    @Query("SELECT * FROM task WHERE task_title LIKE '%' || :queryWord || '%'")
+    fun search(queryWord: String): Flow<List<Task>>
 }
